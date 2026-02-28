6fe2b664e0c25925cccffbbdc94ac8456647931a
Ashtamkatiyar <katiyarashtam014@gmail.com>
2026-02-28 12:34:17 +0530

    Update TestApp.java

--- src/Day4Assignment1/com/labs/TestApp.java
+++ src/Day4Assignment1/com/labs/TestApp.java
@@ -8,17 +8,17 @@ public class TestApp {
         System.out.println("Average Salary: " +
                 EmployeeService.calculateAverageSalary());
 
-//        System.out.println("\nMax Salary By Department:");
-//        System.out.println(EmployeeService.getMaxSalaryByDepartment());
-//
-//        System.out.println("\nTotal Salary By Department:");
-//        System.out.println(EmployeeService.getTotalSalaryByDepartment());
-//
+        System.out.println("\nMax Salary By Department:");
+      System.out.println(EmployeeService.getMaxSalaryByDepartment());
+
+        System.out.println("\nTotal Salary By Department:");
+        System.out.println(EmployeeService.getTotalSalaryByDepartment());
+
         System.out.println("\nEmployee Name & Salary:");
          EmployeeService.displayNameAndSalary();
-//
+
        System.out.println("\nSorted By Salary Desc:");
         System.out.print( EmployeeService.sortSalDesc());
-//
+
     }
 }
